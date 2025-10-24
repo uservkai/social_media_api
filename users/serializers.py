471881668434279f85aeaad1973b9794e18f8496
uservkai <katemary6623@gmail.com>
2025-10-24 22:36:55 +0300

    Add user authentication endpoints and models for registration and login

---
+++ users/serializers.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+from .models import User, Profile, Follow
+from django.contrib.auth import authenticate
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+        Profile.objects.create(user=user)  # auto-create an associated profile
+        return user
+    
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField(max_length=150)
+    password = serializers.CharField(max_length=20, write_only=True)
+    
+    def validate(self, data):
+        user = authenticate(**data)
+        if user and user.is_active:
+            return user
+        raise serializers.ValidationError("Invalid credentials")
+    
